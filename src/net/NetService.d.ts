4762be8f1e04da538dc59617e148d896e8712e82
Brave Chen <brave_cgx@163.com>
2019-05-23 10:39:04 +0800

    change NetService.d.ts change NetService.js add net status message

--- src/net/NetService.d.ts
+++ src/net/NetService.d.ts
@@ -1,25 +1,48 @@
+interface IGoodNet {
+  // 网络状态
+  type: string,
+  // 是否是警告信息
+  isWarning?: boolean,
+  // 信息的标题，仅在警告信息时存在
+  title?: string,
+  // 信息的详情，仅在警告信息时存在
+  content?: string
+}
+
+interface IBadNet {
+  // 错误标题
+  title: string,
+  // 问题详情
+  content?: string
+}
+
 /**
  * @description 网络状态相关服务
  * @author Brave Chan on 2017.8.24
  */
 declare namespace NetService {
+  /**
+   * @public
+   *
+   * @description 开启/关闭 debug模式
+   */
   export let debug: boolean;
   /**
    * @public
    *
    * @description 监控网络状态
-   * @param {() => void} changeToGoodFn [optional] 网络变好时的回调
-   * @param {() => void} changeToBadFn [optional] 网络变差是的回调
+   * @param {(msg: IGoodNet) => void} goodFn [optional] 网络变好时的回调
+   * @param {(msg: IBadNet) => void} badFn [optional] 网络变差是的回调
    */
-  export function checkNet(changeToGoodFn: () => void, changeToBadFn: () => void): void;
+  export function checkNet(goodFn: (msg: IGoodNet) => void, badFn: (msg: IBadNet) => void): void;
   /**
    * @public
    *
    * @description 检测网络状态
-   * @param {Function} goodFn [optional] 网络情况好时调用该回调
-   * @param {Function} badFn [optional] 网络情况不好时调用该回调
+   * @param {(msg: IGoodNet) => void} goodFn [optional] 网络情况好时调用该回调
+   * @param {(msg: IBadNet) => void} badFn [optional] 网络情况不好时调用该回调
    */
-  export function watchNet(goodFn: () => void, badFn: () => void): void;
+  export function watchNet(goodFn: (msg: IGoodNet) => void, badFn: (msg: IBadNet) => void): void;
 }
 
 export default NetService;
--- src/net/NetService.js
+++ src/net/NetService.js
@@ -7,83 +7,85 @@ let _debug = false;
 //===============================================
 const prompt = {
     '2g':function(goodFn,badFn){
-        wx.showModal({
-            title:'网络不畅',
-            content:'您当前的网络环境为2G，加载数据可能会比较慢，请您耐心等候，或者更换到较好的网络环境。',
-            showCancel:false,
-            confirmText:'知道了',
-        });
         if(typeof goodFn === 'function'){
-            goodFn();
+            goodFn({
+              isWarning: true,
+              type: '2g',
+              title:'网络不畅',
+              content:'您当前的网络环境为2G，加载数据可能会比较慢，请您耐心等候，或者更换到较好的网络环境。'
+          });
         }
     },
     'none':function(goodFn,badFn){
-        wx.showModal({
-            title:'网络有问题',
-            content:'您当前网络环境可能出现了问题，数据完全加载不了，请您检查网络连接是否正常，谢谢。',
-            showCancel:false,
-            confirmText:'知道了'
-        });
         if(typeof badFn === 'function'){
-            badFn();
+            badFn({
+              title:'网络有问题',
+              content:'您当前网络环境可能出现了问题，数据完全加载不了，请您检查网络连接是否正常，谢谢。',
+          });
         }
     },
     'wifi':function(goodFn,badFn){
         if(typeof goodFn === 'function'){
-            goodFn();
+            goodFn({
+              type: 'wifi'
+            });
         }
     },
     '3g':function(goodFn,badFn){
         if(typeof goodFn === 'function'){
-            goodFn();
+            goodFn({
+              type: '3g'
+            });
         }
     },
     '4g':function(goodFn,badFn){
         if(typeof goodFn === 'function'){
-            goodFn();
+            goodFn({
+              type: '4g'
+            });
         }
     },
     'unknow':function(goodFn,badFn){
-        wx.showModal({
-            title:'网络环境不明',
-            content:'无法检测您当前所在网络环境类型，如果数据加载出现问题，请您切换到较好的网络环境再使用。',
-            showCancel:false,
-            confirmText:'知道了'
-        });
         if(typeof goodFn === 'function'){
-            goodFn();
+            goodFn({
+              type: 'unknow',
+              isWarning: true,
+              title:'网络环境不明',
+              content:'无法检测您当前所在网络环境类型，如果数据加载出现问题，请您切换到较好的网络环境再使用。'
+          });
         }
     },
+    default(goodFn, badFn) {
+      if (typeof badFn === 'function') {
+        badFn({
+          title:'无法检测网络状态',
+          content:'无法检测您当前所在网络状态，请您检查网络连接是否正常，或者请您切换到较好的网络环境再使用。',
+        });
+      }
+    }
 };
-let _changeToGoodFn,_changeToBadFn;
-let netChangeBad = false;
+
 //================================================
 /**
  * @public
  *
  * 监控网络状态
- * @param {Function} changeToGoodFn 网络变好时的回调
- * @param {Function} changeToBadFn 网络变差是的回调
+ * @param {Function} goodFn 网络变好时的回调
+ * @param {Function} badFn 网络变差是的回调
  */
-function watchNet(changeToGoodFn,changeToBadFn){
-    _changeToGoodFn = changeToGoodFn;
-    _changeToBadFn = changeToBadFn;
+function watchNet(goodFn, badFn){
     if (wx.onNetworkStatusChange) {
         wx.onNetworkStatusChange(function(res){
             if(!res.isConnected){
-                netChangeBad = true;
-                return prompt['none'](null,_changeToBadFn);
+                return prompt['none'](null, badFn);
             }
-            if(netChangeBad){
-                netChangeBad = false;
-            }
-            let fn = prompt[res.networkType];
+
+            let fn = prompt[res.networkType] || prompt.default;
             if(typeof fn === 'function'){
-                fn(_changeToGoodFn,_changeToBadFn);
+                fn(goodFn, badFn);
             }
         });
     } else {
-        // 如果希望用户在最新版本的客户端上体验您的小程序，可以这样子提示
         wx.showModal({
             title: '提示',
             content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。',
@@ -105,7 +107,7 @@ function checkNet(goodFn,badFn){
             // 返回网络类型, 有效值：
             // wifi/2g/3g/4g/unknown(Android下不常见的网络类型)/none(无网络)
             //res.networkType
-            let fn = prompt[res.networkType];
+            let fn = prompt[res.networkType] || prompt.default;
             if(typeof fn === 'function'){
                 fn(goodFn,badFn);
             }
