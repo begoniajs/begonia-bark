2bf8be9f744a9bfc70ac2e4fa9c3cdf8c0902599
Brave Chen <brave_cgx@163.com>
2019-06-21 14:42:11 +0800

    change gulp config change the storage/storage.js to storage/StorageManager.js

--- gulpfile.js/storage.js
+++ gulpfile.js/storage.js
@@ -3,8 +3,8 @@ const { combineRollupConfig, cpTypes } = require('./util');
 
 const config = {
   input: './src/storage/StorageManager.js',
-  fileName: 'Storage',
-  outDir: './dist/storage',
+  fileName: 'StorageManager', // 输出的文件名称
+  outDir: './dist/storage', // 输出的文件夹
   banner: `// bebark/Storage v1.0.0 by Brave Chan`
 };
 
